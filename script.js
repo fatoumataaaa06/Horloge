c3f0bd8f08b7f0454d4f3f770fa3adabf502d7e3
fatoumataaaa06 <diantefatoumata06@gmail.com>
2026-09-01 12:45:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-
+function afficherHeure(){
+  let now=new Date();
+  let heure=now.getHours();
+  let minutes=now.getMinutes();
+  let secondes=now.getSecondes();
+  if(heure<10){
+    heure="0"+heure;
+  }
+    if(minutes<10){
+    minutes="0"+minutes;
+  }
+    if(secondes<10){
+    secondes="0"+secondes;
+  }
+  document.write(heure +":"+ minutes +":"+ secondes);
+}
+  afficherHeure();
+setInterval(afficherHeure(),1000);
+  
